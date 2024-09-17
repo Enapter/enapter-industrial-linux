d1a348b1c3e7fd1102044c81c7d1b41bc63217c5
Dmitry Myaskovskiy <dmitry@enapter.com>
2024-09-17 23:20:38 +0200

    Rauc updates & install initial implementation

--- bin/prepare-environments.sh
+++ bin/prepare-environments.sh
@@ -1,11 +1,15 @@
 # SPDX-FileCopyrightText: 2024 Enapter <developers@enapter.com>
 # SPDX-License-Identifier: Apache-2.0
 
-export BB_ENV_PASSTHROUGH_ADDITIONS="$BB_ENV_PASSTHROUGH_ADDITIONS DISTRO_VERSION DL_DIR SSTATE_DIR TMPDIR SECURE_BOOT_SIGNING_KEY SECURE_BOOT_SIGNING_CERT SECURE_BOOT_SIGNING_CERT_DER"
+export BB_ENV_PASSTHROUGH_ADDITIONS="$BB_ENV_PASSTHROUGH_ADDITIONS DISTRO_VERSION DL_DIR SSTATE_DIR TMPDIR SECURE_BOOT_SIGNING_KEY SECURE_BOOT_SIGNING_CERT SECURE_BOOT_SIGNING_CERT_DER RAUC_KEY RAUC_CERT RAUC_KEYRING"
 
 export SECURE_BOOT_SIGNING_CERT="/home/build/secure_boot_signing/sign.crt"
 export SECURE_BOOT_SIGNING_KEY="/home/build/secure_boot_signing/sign.key"
 export SECURE_BOOT_SIGNING_CERT_DER="/home/build/secure_boot_signing/sign.cer"
+
+export RAUC_CERT="/home/build/rauc/production.crt"
+export RAUC_KEY="/home/build/rauc/production.key"
+export RAUC_KEYRING="/home/build/rauc/ca.cert.pem"
 
 if [ -z "$CI_COMMIT_TAG" ]; then
   export DISTRO_VERSION="${ENAPTER_LINUX_BASE_VERSION}-dev-${CI_PIPELINE_ID:-${CI_COMMIT_SHORT_SHA:-unknown}}"
