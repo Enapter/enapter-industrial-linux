24d9c576fc04c551151f64c329c946121d9c7ebc
Dmitry Myaskovskiy <dmitry@enapter.com>
2024-09-24 19:52:14 +0200

    Cleanup

--- docker/bin/prepare-artifacts.sh
+++ docker/bin/prepare-artifacts.sh
@@ -5,111 +5,123 @@
 set -ex
 
 wic_file="enapter-industrial-linux-image-intel-corei7-64.rootfs.wic"
-vmdk_file="enapter-industrial-linux-image-intel-corei7-64.rootfs.wic.vmdk"
 root_ext4_file="enapter-industrial-linux-rootfs-intel-corei7-64.rootfs.ext4"
-update_file="enapter-industrial-linux-update.zip"
 efi_enapter_dir="EFI/enapter"
 efi_boot_dir="EFI/BOOT"
 rauc_manifest="manifest.raucm"
 install_bundle_name="install.raucb"
+img_path="$deploy_dir/$IMG_FILE_ARTIFACT_NAME"
 
 update_dir=/home/build/update
 rauc_update_dir=/home/build/rauc-update
 images_dir=/home/build/images
 deploy_dir=/home/build/tmp-glibc/deploy/images/intel-corei7-64
+rauc_kernel_files="initrd bzImage"
+rauc_boot_files="unicode.pf2 bootx64.efi mmx64.efi grubx64.efi"
+rauc_bootloader_update="bootloader.vfat"
+rauc_bootloader_update_dir="bootloader"
+rauc_kernel_update="kernel.tar.gz"
+rauc_kernel_update_dir="kernel"
+rauc_rootfs_update="rootfs.ext4"
+rauc_enapter_cert_file="Enapter.cer"
+enapter_files="rootfs.img bzImage initrd version.txt"
+boot_files="grubx64.efi grub.cfg"
 
-cp "$deploy_dir/$wic_file" "$deploy_dir/$IMG_FILE_ARTIFACT_NAME"
-
-img_path="$deploy_dir/$IMG_FILE_ARTIFACT_NAME"
-vmdk_path="$deploy_dir/$vmdk_file"
-
+# Cleanup from previous run
 rm -rf "$update_dir"
 rm -rf "$rauc_update_dir"
 rm -vf "$images_dir/enapter-industrial-linux-"*
 
-mkdir -p "$update_dir"
-enapter_files="rootfs.img bzImage initrd version.txt"
-boot_files="grubx64.efi grub.cfg"
+# Copying industrial Linux img file with another name
+# to make this script easier to test
+cp "$deploy_dir/$wic_file" "$img_path"
 
-for f in $enapter_files; do
-  wic cp "$img_path:1/$efi_enapter_dir/$f" "$update_dir/"
-done
-
-for f in $boot_files; do
-  wic cp "$img_path:1/$efi_boot_dir/$f" "$update_dir/"
-done
-
-rauc_version_file="version.txt"
-rauc_kernel_files="initrd bzImage"
-rauc_boot_files="unicode.pf2 bootx64.efi mmx64.efi grubx64.efi"
-rauc_bootloader_update_dir="bootloader"
-rauc_kernel_update_dir="kernel"
-rauc_rootfs_update="rootfs"
-rauc_enapter_cert_file="Enapter.cer"
-
+# Temporary dirs for industrial Linux update
 mkdir -p "$rauc_update_dir"
 mkdir -p "$rauc_update_dir/$rauc_bootloader_update_dir/EFI/BOOT"
 mkdir -p "$rauc_update_dir/$rauc_kernel_update_dir"
 
-cp "$deploy_dir/$root_ext4_file" "$rauc_update_dir/$rauc_rootfs_update.ext4"
+# Copy build rootfs ext4 image to update folder
+cp "$deploy_dir/$root_ext4_file" "$rauc_update_dir/$rauc_rootfs_update"
 
-wic cp "$img_path:1/$efi_enapter_dir/$rauc_version_file" "/tmp/$rauc_version_file"
-wic cp "$img_path:1/$rauc_enapter_cert_file" "$rauc_update_dir/$rauc_bootloader_update_dir/$rauc_enapter_cert_file"
-
-for f in $rauc_kernel_files; do
-  wic cp "$img_path:1/$efi_enapter_dir/$f" "$rauc_update_dir/$rauc_kernel_update_dir"
-done
-
+# Copy grub stuff into bootloader image temp folder
 for f in $rauc_boot_files; do
   wic cp "$img_path:1/$efi_boot_dir/$f" "$rauc_update_dir/$rauc_bootloader_update_dir/EFI/BOOT"
 done
 
+# Enapter.cer file will be part of bootloader image
+wic cp "$img_path:1/$rauc_enapter_cert_file" "$rauc_update_dir/$rauc_bootloader_update_dir/$rauc_enapter_cert_file"
+
+# Copying bzImage and initrd to kernel update image temp folder
+for f in $rauc_kernel_files; do
+  wic cp "$img_path:1/$efi_enapter_dir/$f" "$rauc_update_dir/$rauc_kernel_update_dir"
+done
+
+# We are using special grub config for system when it is installed on disk
 wic cp "$img_path:1/$efi_boot_dir/grub.cfg.install" "$rauc_update_dir/$rauc_bootloader_update_dir/EFI/BOOT/grub.cfg"
 
-# tar -czf "$rauc_update_dir/$rauc_bootloader_update_dir.tar.gz" -C "$rauc_update_dir/$rauc_bootloader_update_dir" --strip-components 1 .
-mkfs.vfat -n "enp-boot" -C "$rauc_update_dir/$rauc_bootloader_update_dir.vfat" 16384 # 16Mb
-mcopy -i "$rauc_update_dir/$rauc_bootloader_update_dir.vfat" -s "$rauc_update_dir/$rauc_bootloader_update_dir"/* ::
+# Creating vfat image for bootloader
+mkfs.vfat -n "enp-boot" -C "$rauc_update_dir/$rauc_bootloader_update" 16384 # 16Mb
+mcopy -i "$rauc_update_dir/$rauc_bootloader_update" -s "$rauc_update_dir/$rauc_bootloader_update_dir"/* ::
 
-tar -czf "$rauc_update_dir/$rauc_kernel_update_dir.tar.gz" -C "$rauc_update_dir/$rauc_kernel_update_dir" --strip-components 1 .
-
-rm -rf "${rauc_update_dir:?}/$rauc_kernel_update_dir"
-rm -rf "${rauc_update_dir:?}/$rauc_bootloader_update_dir"
-
-version="$(cat "/tmp/$rauc_version_file")"
+# Creating kernel update image
+tar -czf "$rauc_update_dir/$rauc_kernel_update" -C "$rauc_update_dir/$rauc_kernel_update_dir" --strip-components 1 .
 
 cat << EOF > "$rauc_update_dir/$rauc_manifest"
 [update]
 compatible=Enapter Linux
-version=$version
+version=$DISTRO_VERSION
 
 [bundle]
 format=verity
 
 [image.rootfs]
-filename=$rauc_rootfs_update.ext4
+filename=$rauc_rootfs_update
 
 [image.bootloader]
-filename=$rauc_bootloader_update_dir.vfat
+filename=$rauc_bootloader_update
 
 [image.kernel]
-filename=$rauc_kernel_update_dir.tar.gz
+filename=$rauc_kernel_update
 EOF
 
+# Remove temp dir from update bundle folder
+rm -rf "${rauc_update_dir:?}/$rauc_kernel_update_dir"
+rm -rf "${rauc_update_dir:?}/$rauc_bootloader_update_dir"
+
+# Creating update bundle in output folder
 rauc bundle --cert="$RAUC_CERT" --key="$RAUC_KEY" "$rauc_update_dir/" "$images_dir/$RAUC_UPDATE_ARTIFACT_NAME"
 
-# put update bundle as install bundle file inside disk image
+# Put update bundle as install bundle file inside disk image
 wic cp "$images_dir/$RAUC_UPDATE_ARTIFACT_NAME" "$img_path:1/$install_bundle_name"
 
+# Creating temp dir for building legacy update bundle
+mkdir -p "$update_dir"
+
+# Extracting system images from .img into temp folder
+for f in $enapter_files; do
+  wic cp "$img_path:1/$efi_enapter_dir/$f" "$update_dir/"
+done
+
+# Extracting bootloader files and grub.cfg from .img into temp folder
+for f in $boot_files; do
+  wic cp "$img_path:1/$efi_boot_dir/$f" "$update_dir/"
+done
+
+# Legacy update bundle checksums
 cd "$update_dir"
 # shellcheck disable=SC2086
 sha256sum $enapter_files $boot_files > SHA256SUMS
+# Build legacy update bundle (simple zip archive) into output dir
 # shellcheck disable=SC2086
-zip -0 "$update_file" $enapter_files $boot_files SHA256SUMS
+zip -0 "$images_dir/$UPDATE_ARTIFACT_NAME" $enapter_files $boot_files SHA256SUMS
 
+# Calculate .img file checksum
 hashsum=$(sha256sum "$img_path" | awk '{ print $1 }')
 echo "$hashsum  $IMG_FILE_ARTIFACT_NAME" > SHA256SUMS
+# Create zip file with .img and hashsum
 zip -rj "/tmp/$IMG_ARTIFACT_NAME" "$img_path" SHA256SUMS
 
-cp "$update_file" "$images_dir/$UPDATE_ARTIFACT_NAME"
+# .img and .vmdk images are ready
 cp "/tmp/$IMG_ARTIFACT_NAME" "$images_dir/$IMG_ARTIFACT_NAME"
 qemu-img convert -O vmdk "$img_path" "$images_dir/$VMDK_ARTIFACT_NAME"
